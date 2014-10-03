6357d6ea0ebfcc3564fd2d11b018606e8567ae4e
Cezary <cezary.nowak@gmail.com>
2014-10-03 12:36:27 +0200

    better documentation, fixes

--- console.php
+++ console.php
@@ -1,42 +1,72 @@
 <?php
 /*
-	usage:
-	<script src="http://localhost/console/console.php?key=XYZ">
+	simple usage:
+	<script src="http://localhost/console/console.php?key=XYZ"></script>
+
+	make sure if you /logs/ directory is writable.
 */
 
+/**** CONFIGS: ****/
+/*
+	key:	default: default
+			Which log file will be used.
+			If not exist, /log/{$key} will be generated automaticly.
+			Select any key, you want.
+			Dont use special characters like /, \, &, | in your key,
+			because it is filename.
+*/
+$key = isset($_GET['key']) ? $_GET['key']			: 'default';
+/*
+	async:	default: true
+			when it's "false", console.log triggers async request to console.
+*/
+$async = isset($_GET['async']) ? ($_GET['async'] === 'false' ? false : true) : true;
+/*
+	consoleUrl:	default: null
+				when it's not passed, the only way you can insert
+				console.php to your code is: <script src=".."></script>
+				and you can not use async or defer.
+				When it contains console.php url, yo ucan get the script
+				any way, you want.
+*/
+$consoleUrl = isset($_GET['consoleUrl']) ? $_GET['consoleUrl']: null;
 
-$key = isset($_GET['key']) ? $_GET['key'] 			: 'default';
-$method = isset($_GET['method']) ? $_GET['method']	: false;
-$args = isset($_GET['args']) ? $_GET['args']		: false;
-$async = isset($_GET['async']) ? $_GET['async']		: true;
+
+$_args = isset($_GET['_args']) ? $_GET['_args']		: false;
+$_method = isset($_GET['_method']) ? $_GET['_method']	: false;
 
 /* write logs, logToServer target */
-if($method !== false && $args !== false) {
+if($_method !== false && $_args !== false) {
 	$file = "./logs/{$key}.log";
-	$data = date('c') . "|{$method}|{$args}\n";
+	$data = date('c') . "|{$_method}|{$_args}\n";
 	@file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
 	die('OK');
 }
 
+/* default action: get javascript */
 header('Content-Type: application/javascript');
 ?>
+<?php if($consoleUrl): ?>
+var scriptSrc = <?php echo json_encode($consoleUrl); ?>;
+<?php else: ?>
 var scriptSrc = (function() {
 	var scripts = document.getElementsByTagName('script');
 	var thisScriptTag = scripts[scripts.length-1];
 	return thisScriptTag.attributes.src.value;
 })();
+<?php endif; ?>
 
 function httpGet(theUrl) {
     var xmlHttp;
     xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, <?php echo $async ? 'true' : 'false'; ?> );
     xmlHttp.send( null );
-    return xmlHttp.responseText;
+    return xmlHttp;
 }
 
 function logToServer(type, args) {
 	args = JSON.stringify(Array.prototype.slice.call(args));
-	httpGet(scriptSrc + '&method=' + encodeURIComponent(type) + '&args=' + encodeURIComponent(args))
+	httpGet(scriptSrc + '&_method=' + encodeURIComponent(type) + '&_args=' + encodeURIComponent(args))
 }
 
 (function() {
@@ -50,14 +80,14 @@ function logToServer(type, args) {
 	];
 	var origConsole = window.console;
 	window.console = {};
+	var consoleWrapper = function() {
+		var method = Array.prototype.shift.call(arguments);
+		logToServer(method, arguments);
+		return origConsole[method].apply(origConsole, arguments);
+	};
 	for (var i = 0, ml = methods.length; i < ml; i++) {
 		if (window.console[methods[i]]) {
-			window.console[methods[i]] = (function(method) {
-				return function() {
-					logToServer(method, arguments);
-					return origConsole[method].apply(window.console, arguments);
-				};
-			})(methods[i]);
+			window.console[methods[i]] = consoleWrapper.bind(null, methods[i])
 		} else {
 			window.console[methods[i]] = (function(method) {
 				return function() {
@@ -65,5 +95,5 @@ function logToServer(type, args) {
 				};
 			})(methods[i]);
 		}
-	} 
+	}
 })();
